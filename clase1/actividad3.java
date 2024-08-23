6617284800c036525231ffd1f0af81f44e1f988f
ValentinBirstok <valenbirstok22@gmail.com>
2024-08-23 00:38:47 -0300

    actividad3 hecha

---
+++ clase1/actividad3.java
@@ -0,0 +1,21 @@
+package clase1;
+import java.util.Scanner;
+
+public class actividad3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingrese cantidad que quiera sumar");
+        int cantidad=sc.nextInt();
+        System.out.println("La suma de los numeros enteros hasta "+cantidad+" es: "+sumarnumeros(cantidad));
+        
+    }
+    public static int sumarnumeros(int cantidad){
+        if (cantidad==1) {
+            return 1;
+            
+            
+        }
+        return cantidad+sumarnumeros(cantidad-1);
+    }
+    
+}
