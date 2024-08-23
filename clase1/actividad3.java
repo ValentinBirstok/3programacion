45df72772206cd3ff87135f9bdf9bfbcf99a35b9
EzeFroiz <froiz.ezequiel@gmail.com>
2024-08-23 00:55:03 -0300

    Llamadas Recursivas

--- clase1/actividad3.java
+++ clase1/actividad3.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class actividad3 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in); 
+        Scanner sc=new Scanner(System.in);
         System.out.println("Ingrese cantidad que quiera sumar");
         int cantidad=sc.nextInt();
         System.out.println("La suma de los numeros enteros hasta "+ cantidad +" es: "+sumarnumeros(cantidad));
@@ -17,5 +17,13 @@ public class actividad3 {
         }
         return cantidad+sumarnumeros(cantidad-1);
     }
+    //--- ANALISIS DE LA RECURRENCIA DEL ALGORITMO ---
+    //T(n)= T(n-1)+O(1)
+    //T(n)= (T(n-2)+O(1))+O(1)
+    //T(n)= T(n-2)+2*O(1)
+    //T(n)= (T(n-3)+O(1))+2*O(1)
+    //T(n)= T(n-3)+3*O(1)
+    // va incrementando el numero de llamdas recursivas --> O(1)
+    //T(n)= T(0)+n*O(1)
     
 }
