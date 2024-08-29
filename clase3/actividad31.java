9583de3f2f0a9e489d5efd8106a7e3e9f2a06c4d
mstinga1 <stingamateo@gmail.com>
2024-08-29 19:08:23 -0300

    comienzo ejercicio

---
+++ clase3/actividad31.java
@@ -0,0 +1,7 @@
+package clase3;
+
+public class actividad31 {
+    int[] idclientes={143,223,437,843,165};
+    String[] nombreclientes={"juan","martin","alberto","jose","matias"};
+    int[] scoringclientes={10,8,3,4,9};
+}
