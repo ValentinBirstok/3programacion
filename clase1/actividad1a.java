ecc35ed9d92d91652b7d8eea7bcc66adcc37628d
mstinga1 <stingamateo@gmail.com>
2024-08-29 18:59:20 -0300

    cambios finales

--- clase1/actividad1a.java
+++ clase1/actividad1a.java
@@ -3,19 +3,16 @@ package clase1;
 public class actividad1a {
     public static void main(String[] args) {
 
-        int[] array = {1,98,12,3,67,54};  //1
+        int[] array = {1,54,12,95,67,54};  //1
         int maxi = 0;  //1
-
         for (int i=0;i<array.length;i++) {  //1+2(n+1)+n
             if  (array[i] > maxi) {  //2n
                 maxi = array[i];  //n
             }
         }
-
         System.out.println(maxi);  //1
         //f(n)= 1+1+1+2(n+1)+n+2n+n+1
         //f(n)= 4+2n+2+4n
-
         //f(n)= 6n+6
     }
 }
