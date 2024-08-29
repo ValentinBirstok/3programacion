3671e3a6798559c0c6c9703eae73f1046ae543f2
mstinga1 <stingamateo@gmail.com>
2024-08-29 19:27:20 -0300

    cambios y actualizacion final del ejercicio

--- clase3/actividad31.java
+++ clase3/actividad31.java
@@ -4,8 +4,15 @@ public class actividad31 {
     public static void main(String[] args) {
         int[] idclientes={143,223,437,843,165};
         String[] nombreclientes={"juan","martin","alberto","jose","matias"};
-        int[] scoringclientes={10,8,3,4,9};
-        System.out.println(scoremaximo(scoringclientes));
+        int[] scoringclientes={10,8,13,4,9};
+        int scoremaximo=scoremaximo(scoringclientes);
+        System.out.println(scoremaximo);
+
+        for(int i=0;i<scoringclientes.length;i++){
+            if (scoremaximo==scoringclientes[i]){
+                System.out.println("El score maximo es "+scoremaximo+" de "+nombreclientes[i]+" cuyo id es "+idclientes[i]);
+            }
+        }
     }
     private static int scoremaximo(int[] scoringclientes){
         return busqmaximo(scoringclientes,0,scoringclientes.length-1);
@@ -19,6 +26,7 @@ public class actividad31 {
         int izq = busqmaximo(scoringclientes, i, mitad);
         int der = busqmaximo(scoringclientes, mitad + 1, cantidad);  // Ajusta el índice para la mitad derecha
         return Math.max(izq, der);  // Devuelve el máximo de los dos
+    
     }
     
 
