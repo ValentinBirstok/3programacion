1f6157bf4c0787cd701565023c3875981719b0a3
ValentinBirstok <valenbirstok22@gmail.com>
2024-08-29 19:22:34 -0300

    realizo actividad 1

--- clase3/actividad31.java
+++ clase3/actividad31.java
@@ -1,7 +1,25 @@
 package clase3;
 
 public class actividad31 {
-    int[] idclientes={143,223,437,843,165};
-    String[] nombreclientes={"juan","martin","alberto","jose","matias"};
-    int[] scoringclientes={10,8,3,4,9};
+    public static void main(String[] args) {
+        int[] idclientes={143,223,437,843,165};
+        String[] nombreclientes={"juan","martin","alberto","jose","matias"};
+        int[] scoringclientes={10,8,3,4,9};
+        System.out.println(scoremaximo(scoringclientes));
+    }
+    private static int scoremaximo(int[] scoringclientes){
+        return busqmaximo(scoringclientes,0,scoringclientes.length-1);
+
+    }
+    private static int busqmaximo(int[] scoringclientes, int i, int cantidad) {
+        if (i == cantidad) {
+            return scoringclientes[i];  // Devuelve el elemento cuando es el único
+        }
+        int mitad = (cantidad + i) / 2;
+        int izq = busqmaximo(scoringclientes, i, mitad);
+        int der = busqmaximo(scoringclientes, mitad + 1, cantidad);  // Ajusta el índice para la mitad derecha
+        return Math.max(izq, der);  // Devuelve el máximo de los dos
+    }
+    
+
 }
