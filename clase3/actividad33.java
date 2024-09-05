51fbe60016c8a5f32e5fc28e788c6e41d033de4b
EzeFroiz <froiz.ezequiel@gmail.com>
2024-09-05 19:43:58 -0300

    arreglos actividad 33

--- clase3/actividad33.java
+++ clase3/actividad33.java
@@ -3,68 +3,35 @@ package clase3;
 import java.util.Arrays;
 
 public class actividad33 {
-    public static int[] encontrarDosScoringsMaximos(int[] scorings) {
-        // Caso base: si el arreglo tiene solo dos elementos, devolverlos
-        if (scorings.length == 2) {
-            int mayor1, mayor2;
-            if (scorings[0] > scorings[1]) {
-                mayor1 = scorings[0];
-                mayor2 = scorings[1];
-            } else {
-                mayor1 = scorings[1];
-                mayor2 = scorings[0];
+    public class DosScoringsMaximos {
+
+            int[] idclientes = {143, 223, 437, 843, 165};
+            String[] nombreclientes = {"juan", "martin", "alberto", "jose", "matias"};
+            int[] scoringclientes = {10, 8, 13, 4, 9};
+    
+            // Llamada a la función para encontrar los dos máximos
+            int[] dosMaximos = scoreMaximo(scoringclientes);
+    
+            System.out.println("Scorings máximos: " + dosMaximos[0] + " y " + dosMaximos[1]);
+    
+            // Imprimir los detalles de los clientes correspondientes a los dos máximos scorings
+            for (int i = 0; i < scoringclientes.length; i++) {
+                if (dosMaximos[0] == scoringclientes[i]) {
+                    System.out.println("El score máximo es " + dosMaximos[0] + " de " + nombreclientes[i] + " cuyo id es " + idclientes[i]);
+                } else if (dosMaximos[1] == scoringclientes[i]) {
+                    System.out.println("El segundo score máximo es " + dosMaximos[1] + " de " + nombreclientes[i] + " cuyo id es " + idclientes[i]);
+                }
             }
-            return new int[]{mayor1, mayor2};
+    
+        // Función para encontrar los dos máximos scorings
+        private static int[] scoreMaximo(int[] scoringclientes) {
+            return busqDosMaximos(scoringclientes, 0, scoringclientes.length - 1);
         }
-
-        // Si solo hay un número en la lista
-        if (scorings.length == 1) {
-            return new int[]{scorings[0], scorings[0]}; // Si solo hay un número, devolverlo dos veces
-        }
-
-        // Dividir el arreglo en dos mitades
-        int medio = scorings.length / 2;
-        int[] izquierda = Arrays.copyOfRange(scorings, 0, medio);
-        int[] derecha = Arrays.copyOfRange(scorings, medio, scorings.length);
-
-        // Recursivamente encontrar los dos máximos scorings en cada mitad
-        int[] mayoresIzquierda = encontrarDosScoringsMaximos(izquierda);
-        int[] mayoresDerecha = encontrarDosScoringsMaximos(derecha);
-
-        // Combinar los resultados de ambas mitades
-        return combinarDosScoringsMaximos(mayoresIzquierda, mayoresDerecha);
-    }
-
-    // Función para combinar los dos máximos scorings de las dos mitades
-    private static int[] combinarDosScoringsMaximos(int[] izq, int[] der) {
-        int mayor1, mayor2;
-
-        // Encontrar el mayor entre los dos arreglos
-        if (izq[0] > der[0]) {
-            mayor1 = izq[0];
-            if (izq[1] > der[0]) {
-                mayor2 = izq[1];
-            } else {
-                mayor2 = der[0];
-            }
-        } else {
-            mayor1 = der[0];
-            if (der[1] > izq[0]) {
-                mayor2 = der[1];
-            } else {
-                mayor2 = izq[0];
-            }
-        }
-
-        return new int[]{mayor1, mayor2};
-    }
-
-    public static void main(String[] args) {
-        int[] scorings = {10, 20, 5, 3, 100, 50, 25};
-        int[] resultado = encontrarDosScoringsMaximos(scorings);
-        
-        System.out.println("El scoring mayor es: " + resultado[0]);
-        System.out.println("El segundo scoring mayor es: " + resultado[1]);
-    }
-}
-
+    
+        // Función recursiva para encontrar los dos máximos utilizando divide y vencerás
+        private static int[] busqDosMaximos(int[] scoringclientes, int inicio, int fin) {
+            // Caso base: si solo hay un elemento, devolvemos el mismo valor dos veces
+            if (inicio == fin) {
+                return new int[]{scoringclientes[inicio], Integer.MIN_VALUE};
+            }return new int[0];
+}}}
