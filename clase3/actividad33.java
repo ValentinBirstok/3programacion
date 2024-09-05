9c3aec5112e9ffd6e010421be99b6d200aa4920a
mstinga1 <stingamateo@gmail.com>
2024-09-05 19:54:11 -0300

    cambios

--- clase3/actividad33.java
+++ clase3/actividad33.java
@@ -4,69 +4,69 @@ import java.util.Arrays;
 
 public class actividad33 {
 
-public static void main(String[] args) {
-    int[] idclientes = {143, 223, 437, 843, 165};
-    String[] nombreclientes = {"juan", "martin", "alberto", "jose", "matias"};
-    int[] scoringclientes = {10, 8, 13, 4, 9};
+    public static void main(String[] args) {
+        int[] idclientes = {143, 223, 437, 843, 165};
+        String[] nombreclientes = {"juan", "martin", "alberto", "jose", "matias"};
+        int[] scoringclientes = {10, 8, 13, 4, 9};
 
-    // Llamada a la función para encontrar los dos máximos
-    int[] dosMaximos = scoreMaximo(scoringclientes);
+        // Llamada a la función para encontrar los dos máximos
+        int[] dosMaximos = scoreMaximo(scoringclientes);
 
-    System.out.println("Scorings máximos: " + dosMaximos[0] + " y " + dosMaximos[1]);
+        System.out.println("Scorings máximos: " + dosMaximos[0] + " y " + dosMaximos[1]);
 
-    // Imprimir los detalles de los clientes correspondientes a los dos máximos scorings
-    for (int i = 0; i < scoringclientes.length; i++) {
-        if (dosMaximos[0] == scoringclientes[i]) {
-            System.out.println("El score máximo es " + dosMaximos[0] + " de " + nombreclientes[i] + " cuyo id es " + idclientes[i]);
-        } else if (dosMaximos[1] == scoringclientes[i]) {
-            System.out.println("El segundo score máximo es " + dosMaximos[1] + " de " + nombreclientes[i] + " cuyo id es " + idclientes[i]);
+        // Imprimir los detalles de los clientes correspondientes a los dos máximos scorings
+        for (int i = 0; i < scoringclientes.length; i++) {
+            if (dosMaximos[0] == scoringclientes[i]) {
+                System.out.println("El score máximo es " + dosMaximos[0] + " de " + nombreclientes[i] + " cuyo id es " + idclientes[i]);
+            } else if (dosMaximos[1] == scoringclientes[i]) {
+                System.out.println("El segundo score máximo es " + dosMaximos[1] + " de " + nombreclientes[i] + " cuyo id es " + idclientes[i]);
+            }
         }
     }
-}
 
-// Función para encontrar los dos máximos scorings
-private static int[] scoreMaximo(int[] scoringclientes) {
-    return busqDosMaximos(scoringclientes, 0, scoringclientes.length - 1);
-}
-
-// Función recursiva para encontrar los dos máximos utilizando divide y vencerás
-private static int[] busqDosMaximos(int[] scoringclientes, int inicio, int fin) {
-    // Caso base: si solo hay un elemento, devolvemos el mismo valor dos veces
-    if (inicio == fin) {
-        return new int[]{scoringclientes[inicio], Integer.MIN_VALUE};
+    // Función para encontrar los dos máximos scorings
+    private static int[] scoreMaximo(int[] scoringclientes) {
+        return busqDosMaximos(scoringclientes, 0, scoringclientes.length - 1);
     }
 
-    // Caso base: si hay dos elementos, devolvemos ambos ordenados
-    if (fin - inicio == 1) {
-        if (scoringclientes[inicio] > scoringclientes[fin]) {
-            return new int[]{scoringclientes[inicio], scoringclientes[fin]};
+    // Función recursiva para encontrar los dos máximos utilizando divide y vencerás
+    private static int[] busqDosMaximos(int[] scoringclientes, int inicio, int fin) {
+        // Caso base: si solo hay un elemento, devolvemos ese elemento dos veces
+        if (inicio == fin) {
+            return new int[]{scoringclientes[inicio], scoringclientes[inicio]};
+        }
+
+        // Caso base: si hay dos elementos, devolvemos ambos ordenados
+        if (fin - inicio == 1) {
+            if (scoringclientes[inicio] > scoringclientes[fin]) {
+                return new int[]{scoringclientes[inicio], scoringclientes[fin]};
+            } else {
+                return new int[]{scoringclientes[fin], scoringclientes[inicio]};
+            }
+        }
+
+        // Dividir el arreglo en dos mitades
+        int mitad = (inicio + fin) / 2;
+        int[] izqMaximos = busqDosMaximos(scoringclientes, inicio, mitad);
+        int[] derMaximos = busqDosMaximos(scoringclientes, mitad + 1, fin);
+
+        // Combinar los dos máximos de cada mitad
+        return combinarDosMaximos(izqMaximos, derMaximos);
+    }
+
+    // Función para combinar los dos máximos de cada mitad
+    private static int[] combinarDosMaximos(int[] izq, int[] der) {
+        int mayor1, mayor2;
+
+        // Determinar el mayor entre los dos arreglos
+        if (izq[0] > der[0]) {
+            mayor1 = izq[0];
+            mayor2 = Math.max(izq[1], der[0]);
         } else {
-            return new int[]{scoringclientes[fin], scoringclientes[inicio]};
+            mayor1 = der[0];
+            mayor2 = Math.max(der[1], izq[0]);
         }
+
+        return new int[]{mayor1, mayor2};
     }
-
-    // Dividir el arreglo en dos mitades
-    int mitad = (inicio + fin) / 2;
-    int[] izqMaximos = busqDosMaximos(scoringclientes, inicio, mitad);
-    int[] derMaximos = busqDosMaximos(scoringclientes, mitad + 1, fin);
-
-    // Combinar los dos máximos de cada mitad
-    return combinarDosMaximos(izqMaximos, derMaximos);
-}
-
-// Función para combinar los dos máximos de cada mitad
-private static int[] combinarDosMaximos(int[] izq, int[] der) {
-    int mayor1, mayor2;
-
-    // Determinar el mayor de los dos
-    if (izq[0] > der[0]) {
-        mayor1 = izq[0];
-        mayor2 = Math.max(izq[1], der[0]);
-    } else {
-        mayor1 = der[0];
-        mayor2 = Math.max(der[1], izq[0]);
-    }
-
-    return new int[]{mayor1, mayor2};
-}
 }
