86c655100e60a5516cf3f340d5769e5a5500d3e6
ValentinBirstok <valenbirstok22@gmail.com>
2024-09-30 18:52:38 -0300

    creo main punto 1

--- clase4/actividad4p1.java
+++ clase4/actividad4p1.java
@@ -1,5 +1,20 @@
 package clase4;
+import java.util.Arrays;
+import java.util.Arrays;
 
 public class actividad4p1 {
-    
+    public static void main(String[] args) {
+        int[] monedas = {1, 5, 10, 25};
+        int monto = 36;
+        boolean resultado = esCambioPosible(monedas, monto);
+        if (resultado) {
+            System.out.println("Es posible entregar el cambio exacto.");
+        } else {
+            System.out.println("No es posible entregar el cambio exacto.");
+        }
+    }
 }
+
+    
+    
+
