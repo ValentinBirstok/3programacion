269c1878ed148fe3e2af25d901410e26c38ec74f
ValentinBirstok <valenbirstok22@gmail.com>
2024-09-30 19:10:39 -0300

    empiezo punto 2

--- clase4/actividad42.java
+++ clase4/actividad42.java
@@ -1,5 +1,22 @@
 package clase4;
 
-public class actividad42 {
-    
-}
+import java.util.Arrays;
+
+public class Actividad42 {
+
+    // Método para encontrar el número mínimo de comprobantes para realizar la compra
+    public static int minComprobantesGreedy(int valorCompra, int[] comprobantes) {
+        // Ordenamos los comprobantes de menor a mayor (orden ascendente)
+        Arrays.sort(comprobantes);
+
+        int comprobantesUsados = 0;
+        int resto = valorCompra;
+
+        // Recorremos el array desde el final (comprobantes de mayor valor)
+        for (int i = comprobantes.length - 1; i >= 0; i--) {
+            // Usamos tantos comprobantes de este tipo como sea posible
+            while (resto >= comprobantes[i]) {
+                resto -= comprobantes[i];
+                comprobantesUsados++;
+            }
+        }
