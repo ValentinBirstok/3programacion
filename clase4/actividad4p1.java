f6b9b485c2203391f8c4c7b5aaf9ea419f3ad161
EzeFroiz <froiz.ezequiel@gmail.com>
2024-09-30 18:57:27 -0300

    actividad1

--- clase4/actividad4p1.java
+++ clase4/actividad4p1.java
@@ -3,6 +3,17 @@ import java.util.Arrays;
 import java.util.Arrays;
 
 public class actividad4p1 {
+    
+    public static boolean esCambioPosible(int[] monedas, int monto) {
+        Arrays.sort(monedas); 
+        for (int i = monedas.length - 1; i >= 0; i--) {
+            while (monto >= monedas[i]) {
+                monto -= monedas[i]; 
+            }
+        }
+        return monto == 0; 
+    }
+
     public static void main(String[] args) {
         int[] monedas = {1, 5, 10, 25};
         int monto = 36;
@@ -12,13 +23,6 @@ public class actividad4p1 {
         } else {
             System.out.println("No es posible entregar el cambio exacto.");
         }
-        return monto == 0; // Retornar verdadero si se ha alcanzado el monto exacto
-    }
-
-    public static void main(String[] args) {
-        int[] monedas = {1, 5, 10, 25};
-        int monto = 36;
-        boolean resultado = esCambioPosible(monedas, monto);
     }
 }
 
