1b4fa5a3193606127c531bbe6315b76146e77bd6
mstinga1 <stingamateo@gmail.com>
2024-10-01 18:43:47 -0300

    continuacion fuerza bruta

--- clase6/actividad61fuerzabruta.java
+++ clase6/actividad61fuerzabruta.java
@@ -10,4 +10,18 @@ public class actividad61fuerzabruta {
         int mejorValor = 0; // Mejor valor encontrado
         String mejorCombinacion = ""; // Combinación que da el mejor valor
     
+         // Evaluar todas las combinaciones de jugadores
+         for (int i = 0; i < (1 << n); i++) { // 2^n combinaciones
+            int costoTotal = 0;
+            int valorTotal = 0;
+            String combinacion = "Comprar: ";
+
+            for (int j = 0; j < n; j++) {
+                // Verificar si el jugador j está incluido en la combinación
+                if ((i & (1 << j)) > 0) {
+                    costoTotal += costos[j];
+                    valorTotal += valores[j];
+                    combinacion += "Jugador " + (j + 1) + ", "; // Concatenar cadena
+                }
+            }
 }
