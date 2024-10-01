bccca99f6f79a12d189649f185dd26b011a643e3
mstinga1 <stingamateo@gmail.com>
2024-10-01 19:51:40 -0300

    realizo greedy actividad 64

---
+++ actividad64greedy.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Paquete {
+    int costo;
+    int ganancia;
+
+    Paquete(int costo, int ganancia) {
+        this.costo = costo;
+        this.ganancia = ganancia;
+    }
+
+    double getRatio() {
+        return (double) ganancia / costo;
+    }
+}
+
+public class actividad64greedy {
+    public static void main(String[] args) {
+        Paquete[] paquetes = {
+            new Paquete(12, 150),
+            new Paquete(20, 200),
+            new Paquete(15, 100),
+            new Paquete(25, 300)
+        };
+        int presupuesto = 35;
+
+        Arrays.sort(paquetes, Comparator.comparingDouble(Paquete::getRatio).reversed());
+
+        int gananciaTotal = 0;
+        for (Paquete paquete : paquetes) {
+            if (presupuesto >= paquete.costo) {
+                presupuesto -= paquete.costo;
+                gananciaTotal += paquete.ganancia;
+            }
+        }
+
+        System.out.println("Ganancia máxima: " + gananciaTotal);
+    }
+}
