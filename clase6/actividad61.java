e91f47572ac7bcd2f2c271620465d1143b154e36
mstinga1 <stingamateo@gmail.com>
2024-10-01 18:28:32 -0300

    inicio actividad 1

--- clase6/actividad61.java
+++ clase6/actividad61.java
@@ -1,5 +1,17 @@
 package clase6;
 
 public class actividad61 {
+        public static void main(String[] args) {
+            int[] pesos = {3, 4, 2}; // Pesos de los objetos
+            int[] valores = {4, 5, 3}; // Valores de los objetos
+            int capacidad = 6; // Capacidad de la mochila
+    
+            int resultado = knapsack(valores, pesos, capacidad);
+            System.out.println("Valor máximo en la mochila: " + resultado);
+        }
+    
+        public static int knapsack(int[] valores, int[] pesos, int capacidad) {
+            int n = valores.length;
+            int[][] dp = new int[n + 1][capacidad + 1];
     
 }
