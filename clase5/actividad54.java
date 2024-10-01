f8559a189215eb9339dc7071e3a05d097577f0fe
mstinga1 <stingamateo@gmail.com>
2024-10-01 17:38:50 -0300

    comienzo ejercicio 4

--- clase5/actividad54.java
+++ clase5/actividad54.java
@@ -2,4 +2,19 @@ package clase5;
 
 public class actividad54 {
     
+        // Grafo representado como lista de adyacencia
+        private final Map<Integer, List<Arista>> grafo;
+    
+        // Constructor
+        public Dijkstra() {
+            this.grafo = new HashMap<>();
+        }
+    
+        // Método para añadir una arista al grafo
+        public void agregarArista(int origen, int destino, int peso) {
+            this.grafo.putIfAbsent(origen, new ArrayList<>());
+            this.grafo.putIfAbsent(destino, new ArrayList<>());
+            this.grafo.get(origen).add(new Arista(destino, peso));
+        }
+
 }
