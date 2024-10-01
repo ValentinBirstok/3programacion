8d7be31509e24bc1dabda4ee0ffbbbad8c2b8463
EzeFroiz <froiz.ezequiel@gmail.com>
2024-10-01 18:30:13 -0300

    actividad 6.1

--- clase6/actividad61.java
+++ clase6/actividad61.java
@@ -13,5 +13,20 @@ public class actividad61 {
         public static int knapsack(int[] valores, int[] pesos, int capacidad) {
             int n = valores.length;
             int[][] dp = new int[n + 1][capacidad + 1];
+
+            // Llenar la tabla dp
+            for (int i = 0; i <= n; i++) {
+                for (int w = 0; w <= capacidad; w++) {
+                    if (i == 0 || w == 0) {
+                        dp[i][w] = 0; // Base: sin objetos o sin capacidad
+                    } else if (pesos[i - 1] <= w) {
+                        dp[i][w] = Math.max(valores[i - 1] + dp[i - 1][w - pesos[i - 1]], dp[i - 1][w]);
+                    } else {
+                        dp[i][w] = dp[i - 1][w]; // No incluir el objeto
+                    }
+                }
+            }
     
-}
+            return dp[n][capacidad]; // Valor máximo
+        }
+    }
