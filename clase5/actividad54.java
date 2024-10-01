95db49ca5bfa9c2ba2cdad8d8e5d915e639ce4b6
ValentinBirstok <valenbirstok22@gmail.com>
2024-10-01 17:42:21 -0300

    implementacion punto 4

--- clase5/actividad54.java
+++ clase5/actividad54.java
@@ -1,5 +1,7 @@
 package clase5;
 
+import clase5.actividad53.Arista;
+
 public class actividad54 {
     
         // Grafo representado como lista de adyacencia
@@ -16,5 +18,30 @@ public class actividad54 {
             this.grafo.putIfAbsent(destino, new ArrayList<>());
             this.grafo.get(origen).add(new Arista(destino, peso));
         }
+        // Implementación del algoritmo de Dijkstra
+        public void dijkstra(int s) {
+        // Inicialización
+            int numVertices = grafo.size();
+            int[] dist = new int[numVertices]; // Distancias desde el origen
+            int[] prev = new int[numVertices]; // Predecesores de cada vértice
+            boolean[] inMST = new boolean[numVertices]; // Verifica si el vértice está en el MST
+
+            // Inicializar distancias a ∞ y prev a NIL
+            Arrays.fill(dist, Integer.MAX_VALUE);
+            Arrays.fill(prev, -1);
+            dist[s] = 0; // La distancia al vértice de origen es 0
+
+            // Crear una cola de prioridad Q y añadir todos los vértices
+            PriorityQueue<Nodo> Q = new PriorityQueue<>(Comparator.comparingInt(nodo -> nodo.distancia));
+            for (int v : grafo.keySet()) {
+                Q.add(new Nodo(v, dist[v]));
+            }
+
+            // Mientras la cola no esté vacía
+            while (!Q.isEmpty()) {
+                // Extraer el vértice u con la distancia mínima
+                Nodo nodoActual = Q.poll();
+                int u = nodoActual.vertice;
+
 
 }
