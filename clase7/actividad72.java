2555b290b1569a7cdbe6b2adadb2c637999570fd
ValentinBirstok <valenbirstok22@gmail.com>
2024-10-02 19:17:57 -0300

    empiezo punto 2

--- clase7/actividad72.java
+++ clase7/actividad72.java
@@ -1,8 +1,27 @@
 package clase7;
 
 public class actividad72 {
-    public static void main(String[] args) {
+    final static int INF = 99999; // Usamos un valor grande para representar el infinito
+
+   l
+    void floydWarshall(int graph[][], int V) {
+        int dist[][] = new int[V][V]; // Crear matriz de distancias
+
+        // Inicializar la matriz de distancias con los valores iniciales del grafo
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                dist[i][j] = graph[i][j]; // Copiamos los valores iniciales
+            }
+        }
+
         
-        
-    }
-}
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    // Si encontramos un camino más corto pasando por el nodo 'k', actualizamos
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
