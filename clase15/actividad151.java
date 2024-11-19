ea7efc25a9f1da2d8d639cf0fa14f49ab43fe5f7
mstinga1 <stingamateo@gmail.com>
2024-11-19 19:30:52 -0300

    realizo act 2

--- clase15/actividad151.java
+++ clase15/actividad151.java
@@ -9,6 +9,16 @@ Complejidad del Problema
 𝑁
 N es eñ numero de ciudades, el número de rutas posibles es: (n-1)!/2
 
+2) (15-1)!/2=43,589,145,600 rutas posibles.
+
+3
+Algoritmo del vecino más cercano
+este método selecciona la ciudad más cercana que aún no haya sido visitada comienza en una ciudad y va construyendo la ruta paso a paso eligiendo siempre la ciudad más próxima no visitada hasta que todas las ciudades hayan sido visitadas
+es rápido y fácil de implementar pero no siempre garantiza la solución óptima
+
+Inserción más cercana
+a partir de una ruta parcial inserta la ciudad no visitada que está más cerca de las ciudades en la ruta actual minimizando la distancia total este método trata de construir una ruta completa eligiendo la mejor opción en cada paso
+
 
                                                                */
 }
