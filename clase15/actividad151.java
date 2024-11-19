9a736dafafd57aa48ff7e04b7279c3208239ffd0
ValentinBirstok <valenbirstok22@gmail.com>
2024-11-19 19:24:51 -0300

    empiezo actividad 1

--- clase15/actividad151.java
+++ clase15/actividad151.java
@@ -1,5 +1,14 @@
 package clase15;
 
 public class actividad151 {
-   
+   /*El Problema del Viajante (TSP, por sus siglas en inglés) es un problema clásico de optimización que se enmarca dentro de la teoría de grafos y la computación. La premisa es simple:
+
+"Dado un conjunto de ciudades y la distancia entre cada par de ciudades, se busca encontrar la ruta más corta que permita visitar todas las ciudades exactamente una vez y regresar a la ciudad de origen."
+Complejidad del Problema
+
+𝑁
+N es eñ numero de ciudades, el número de rutas posibles es: (n-1)!/2
+
+
+                                                               */
 }
