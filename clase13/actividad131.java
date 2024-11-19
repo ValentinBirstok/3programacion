e46f39dd28161b3293d2dcc9897f472e4d6f7ec5
ValentinBirstok <valenbirstok22@gmail.com>
2024-11-19 17:34:08 -0300

    actividad1

---
+++ clase13/actividad131.java
@@ -0,0 +1,15 @@
+package clase13;
+
+public class actividad131 {
+    
+ /*1) . Ejemplos de Acciones posibles del atacante
+-Inyección SQL: El atacante intenta manipular la base de datos introduciendo consultas maliciosas a través de formularios web.
+-Fuerza bruta de contraseñas: Intentos repetidos de adivinar la contraseña de una cuenta.
+-Ataque de denegación de servicio (DoS): Enviar una gran cantidad de solicitudes para saturar el servidor y dejarlo inoperativo.
+-Phishing: Intentar engañar a los usuarios para que proporcionen información confidencial mediante páginas falsas.
+-Acceso no autorizado: Intentar explotar vulnerabilidades para acceder a áreas restringidas sin permiso.
+
+2) Funciones del Sistema de Detección
+
+    
+}*/
