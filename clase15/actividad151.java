66626a821a4d186e27fa2ee0e7b2c8fa72378ea9
mstinga1 <stingamateo@gmail.com>
2024-11-19 19:31:06 -0300

    realizo act 3

--- clase15/actividad151.java
+++ clase15/actividad151.java
@@ -11,7 +11,7 @@ N es eñ numero de ciudades, el número de rutas posibles es: (n-1)!/2
 
 2) (15-1)!/2=43,589,145,600 rutas posibles.
 
-3
+3)
 Algoritmo del vecino más cercano
 este método selecciona la ciudad más cercana que aún no haya sido visitada comienza en una ciudad y va construyendo la ruta paso a paso eligiendo siempre la ciudad más próxima no visitada hasta que todas las ciudades hayan sido visitadas
 es rápido y fácil de implementar pero no siempre garantiza la solución óptima
